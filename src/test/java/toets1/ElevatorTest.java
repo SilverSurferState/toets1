7eabedeccaf036c3540862f80eddfe975386a1da
beto <beto@asuvorm.be>
2022-05-04 19:59:49 +0200

    second test with personel badge

--- src/test/java/toets1/ElevatorTest.java
+++ src/test/java/toets1/ElevatorTest.java
@@ -11,4 +11,10 @@ public class ElevatorTest {
         Elevator elevator = new Elevator("405", 0);
         assertArrayEquals(new Integer[]{4, 10}, elevator.activeButtons());
     }
+
+    @Test
+    void testPersonelBadge(){
+        Elevator elevator = new Elevator("p", -1);
+        assertArrayEquals(new Integer[]{-2,0,1,2,3,4,5,6,7,8,9,10}, elevator.activeButtons());
+    }
 }
